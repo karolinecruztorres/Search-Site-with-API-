10e63e4b0e13f2c3ce892d1bd5c18c83ab2090bb
Karoline Cruz <karollcruz@hotmail.com>
2023-11-24 18:32:45 +0000

    Components and layout for the error page created. #4

--- HPSearchSite/src/App.tsx
+++ HPSearchSite/src/App.tsx
@@ -1,7 +1,36 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import styled from "styled-components";
 import FormWrapper from "./components/FormWrapper";
 import Card from "./components/Card";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 90vh;
+`;
+
+const StyledHeading = styled.h1`
+  margin: 0 0 30px 0;
+  font-size: 48px;
+`;
+
+const StyledButton = styled.button`
+  font-family: "Bigelow Rules", serif;
+  font-size: 32px;
+  border: none;
+  padding: 3px 230px;
+  background-color: rgba(183, 156, 109, 0.5);
+  cursor: pointer;
+  transition: opacity 0.15s, font-weight 0.15s;
+
+  &:hover {
+    opacity: 0.8;
+    font-weight: bold;
+  }
+`;
 
 interface Character {
   actor: string;
@@ -10,7 +39,10 @@ interface Character {
 }
 
 function App() {
-  const [selectedCharacter, setSelectedCharacter] = useState<Character | null>(null);
+  const [selectedCharacter, setSelectedCharacter] = useState<Character | null>(
+    null
+  );
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     fetch("https://hp-api.onrender.com/api/characters")
@@ -20,18 +52,29 @@ function App() {
       })
       .catch((error) => {
         console.error(error);
+        setError("Unable to connect with the Ministry of Magic.");
       });
   }, []);
 
   return (
     <>
-      <FormWrapper  />
-      {selectedCharacter && (
-        <Card
-        character={selectedCharacter.name}
-        image={selectedCharacter.image}
-        actor={selectedCharacter.actor}
-        />
+      {error ? (
+        <StyledDiv>
+          <StyledHeading> {error} </StyledHeading>
+          <StyledButton type="submit"> Try connection </StyledButton>
+        </StyledDiv>
+      ) : (
+        selectedCharacter && (
+          <>
+            <FormWrapper />
+
+            <Card
+              character={selectedCharacter.name}
+              image={selectedCharacter.image}
+              actor={selectedCharacter.actor}
+            />
+          </>
+        )
       )}
     </>
   );
